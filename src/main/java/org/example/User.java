d29e3247d36490b814d4393950c2d503ce0606fe
michael <fhb221263@fh-vie.ac.at>
2025-04-05 17:19:27 +0200

    feat: User table

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String name;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'';
+    }
+}
