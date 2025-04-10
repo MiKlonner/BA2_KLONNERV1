11a657453c1237676255e65cb625924fe1783b0c
michael <fhb221263@fh-vie.ac.at>
2025-04-10 16:54:54 +0200

    feat: add constructer + toString for role

--- src/main/java/org/example/Role.java
+++ src/main/java/org/example/Role.java
@@ -1,7 +1,25 @@
 package org.example;
 
 public enum Role {
-    ADMIN,
-    GAST,
-    USER
+    ADMIN ("ADMIN"),
+    GUEST ("GAST"),
+    USER ("USER");
+
+
+
+
+    private final String role;
+
+    Role(String role) {
+        this.role = role;
+    }
+    public String getRole(Role role) {
+        return this.role;
+    }
+    @Override
+    public String toString() {
+        return "Role{" +
+                "role='" + role + '\'' +
+                '}';
+    }
 }
